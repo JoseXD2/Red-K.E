501a7ee7e155882603c735e053100c185f5f69bb
Josenene8 <100051827+Josenene8@users.noreply.github.com>
2022-06-04 10:05:05 -0300

    Add files via upload

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1184,9 +1184,7 @@ class PlayState extends MusicBeatState
 		kadeEngineWatermark.cameras = [camHUD];
 		if (loadRep)
 			replayTxt.cameras = [camHUD];
-                #if android
-	        addAndroidControls();
-	#end
+
 		// if (SONG.song == 'South')
 		// FlxG.camera.alpha = 0.7;
 		// UI_camera.zoom = 1;
@@ -1350,11 +1348,7 @@ class PlayState extends MusicBeatState
 	#end
 
 	function startCountdown():Void
-	{     
-		#if android
-	        androidc.visible = true;
-	        #end
-			
+	{
 		inCutscene = false;
 
 		/*generateStaticArrows(0);
@@ -2813,14 +2807,14 @@ class PlayState extends MusicBeatState
 		if (isStoryMode)
 			campaignMisses = misses;
 
-		/* if (!loadRep)
+		if (!loadRep)
 			rep.SaveReplay(saveNotes, saveJudge, replayAna);
 		else
 		{
 			PlayStateChangeables.botPlay = false;
 			PlayStateChangeables.scrollSpeed = 1;
 			PlayStateChangeables.useDownscroll = false;
-		}*/
+		}
 
 		if (FlxG.save.data.fpsCap > 290)
 			(cast (Lib.current.getChildAt(0), Main)).setFPSCap(290);
@@ -2898,10 +2892,9 @@ class PlayState extends MusicBeatState
 					StoryMenuState.weekUnlocked[Std.int(Math.min(storyWeek + 1, StoryMenuState.weekUnlocked.length - 1))] = true;
 
 					if (SONG.validScore)
-					{       #if newgrounds
+					{
 						NGio.unlockMedal(60961);
 						Highscore.saveWeekScore(storyWeek, campaignScore, storyDifficulty);
-					        #end
 					}
 
 					FlxG.save.data.weekUnlocked = StoryMenuState.weekUnlocked;
@@ -3616,7 +3609,12 @@ class PlayState extends MusicBeatState
 					releaseArray = [false, false, false, false];
 				} 
 
-			
+				var anas:Array<Ana> = [null,null,null,null];
+
+				for (i in 0...pressArray.length)
+					if (pressArray[i])
+						anas[i] = new Ana(Conductor.songPosition, null, false, "miss", i);
+
 				// HOLDS, check for sustain notes
 				if (holdArray.contains(true) && /*!boyfriend.stunned && */ generatedMusic)
 				{
@@ -3626,10 +3624,12 @@ class PlayState extends MusicBeatState
 							goodNoteHit(daNote);
 					});
 				}
-	
-				// PRESSES, check for note hits
-				if (pressArray.contains(true) && generatedMusic)
+		 
+				if (KeyBinds.gamepad && !FlxG.keys.justPressed.ANY)
 				{
+					// PRESSES, check for note hits
+					if (pressArray.contains(true) && generatedMusic)
+					{
 						boyfriend.holdTimer = 0;
 			
 						var possibleNotes:Array<Note> = []; // notes that can be hit
@@ -3697,7 +3697,9 @@ class PlayState extends MusicBeatState
 										mashViolations--;
 									scoreTxt.color = FlxColor.WHITE;
 									var noteDiff:Float = -(coolNote.strumTime - Conductor.songPosition);
-							
+									anas[coolNote.noteData].hit = true;
+									anas[coolNote.noteData].hitJudge = Ratings.CalculateRating(noteDiff, Math.floor((PlayStateChangeables.safeFrames / 60) * 1000));
+									anas[coolNote.noteData].nearestNote = [coolNote.strumTime,coolNote.noteData,coolNote.sustainLength];
 									goodNoteHit(coolNote);
 								}
 							}
@@ -3710,10 +3712,10 @@ class PlayState extends MusicBeatState
 							}
 					}
 
-					/*if (!loadRep)
+					if (!loadRep)
 						for (i in anas)
 							if (i != null)
-								replayAna.anaArray.push(i); // put em all there*/
+								replayAna.anaArray.push(i); // put em all there
 				}
 				notes.forEachAlive(function(daNote:Note)
 				{
@@ -3821,7 +3823,7 @@ class PlayState extends MusicBeatState
 
 			public function backgroundVideo(source:String) // for background videos
 				{
-					 /*#if cpp
+					#if cpp
 					useVideo = true;
 			
 					FlxG.stage.window.onFocusOut.add(focusOut);
@@ -3856,23 +3858,23 @@ class PlayState extends MusicBeatState
 			
 					videoSprite = new FlxSprite(-470,-30).loadGraphic(data);
 			
-					videoSprite.setGraphicSize(Std.int(videoSprite.width * 1.2));*/
+					videoSprite.setGraphicSize(Std.int(videoSprite.width * 1.2));
 			
 					remove(gf);
 					remove(boyfriend);
 					remove(dad);
-				//	add(videoSprite);
+					add(videoSprite);
 					add(gf);
 					add(boyfriend);
 					add(dad);
 			
-					/* trace('poggers');
+					trace('poggers');
 			
 					if (!songStarted)
 						webmHandler.pause();
 					else
 						webmHandler.resume();
-					#end*/
+					#end
 				}
 
 	function noteMiss(direction:Int = 1, daNote:Note):Void
